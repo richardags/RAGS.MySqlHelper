9166c0b30f6a2f10e9069b76d9b6c72447beadfd
richardags <richardags@hotmail.com>
2022-10-08 04:02:16 -0400

    Add project files.

---
+++ MySqlHelper/MySqlHelperNS.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MySqlHelper
+{
+    public class MySqlHelperNS
+    {
+        private readonly string connectionString;
+
+        public MySqlHelperNS(string host, string database, string username, string password)
+        {
+            connectionString = string.Format(
+                "SERVER={0}; DATABASE={1}; UID={2}; PWD={3}; PORT=; Allow User Variables=True;SSL Mode=None;",
+                host, database, username, password);
+        }
+
+        public MySqlHelperResult Execute(string query, bool readData, Dictionary<string, object> parameters = null)
+        {
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    mySqlConnection.Open();
+                    MySqlTransaction mySqlTransaction = mySqlConnection.BeginTransaction();
+
+                    try
+                    {
+                        using (MySqlCommand mySqlCommand = new MySqlCommand(query, mySqlConnection, mySqlTransaction))
+                        {
+                            if (parameters != null && parameters.Count > 0)
+                            {
+                                foreach (KeyValuePair<string, object> parameter in parameters)
+                                {
+                                    mySqlCommand.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                                }
+                            }
+                            if (readData)
+                            {
+                                DataTable result = null;
+
+                                using (MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader())
+                                {
+                                    if (mySqlDataReader.HasRows)
+                                    {
+                                        result = new DataTable();
+                                        result.Load(mySqlDataReader);
+                                    }
+                                }
+
+                                mySqlTransaction.Commit();
+                                return new MySqlHelperResult(data: result?.CreateDataReader());
+                            }
+                            else
+                            {
+                                int affected_rows = mySqlCommand.ExecuteNonQuery();
+                                mySqlTransaction.Commit();
+                                return new MySqlHelperResult(affected_rows: affected_rows);
+                            }
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        mySqlTransaction.Rollback();
+                        return new MySqlHelperResult(error: exception);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    return new MySqlHelperResult(error: exception);
+                }
+            }
+        }
+        public static string EscapeString(string value)
+        {
+            return MySql.Data.MySqlClient.MySqlHelper.EscapeString(value);
+        }
+    }
+}
---
+++ MySqlHelper/MySqlHelperResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MySqlHelper
+{
+    public class MySqlHelperResult
+    {
+        public Exception error;
+        public object data;
+        public int affected_rows;
+
+        public MySqlHelperResult(object data)
+        {
+            this.data = data;
+        }
+        public MySqlHelperResult(int affected_rows)
+        {
+            this.affected_rows = affected_rows;
+        }
+        public MySqlHelperResult(Exception error)
+        {
+            this.error = error;
+        }
+    }
+}
